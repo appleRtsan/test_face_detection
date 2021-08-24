55d00be68568679eee4d5d862b3389a69c097f16
appleRtsan <a29947393@gmail.com>
2021-08-24 14:52:44 +0800

    let the detection work smoothly

--- openface_mtcnn/testmtcnn.py
+++ openface_mtcnn/testmtcnn.py
@@ -7,16 +7,16 @@ import cv2
 
 one_size = 500
 
-def get_data_from_webcam(image):
+def get_data_from_webcam(image,detector):
 
-    detector = mtcnn.MTCNN()
     faces = detector.detect_faces(image)
 
     if (len(faces) == 0):
-        print("?")
         return None
     left_eye_img = []
     right_eye_img = []
+    got_left = False
+    got_right = False
     for face in faces:
         x, y, w, h = face['box']
         if w < 50 or h < 25:
@@ -31,24 +31,29 @@ def get_data_from_webcam(image):
                 left_eye_img = image[value[1]-25:value[1]+25, value[0]-25:value[0]+25]
                 # cv2.imshow('leye',left_eye_img)
                 cv2.rectangle(image,(value[0]-25, value[1]-25), (value[0]+25, value[1]+25), (0, 255, 0), 2)
+                got_left = True
             elif key == 'right_eye':
                 right_eye_img = image[value[1]-25:value[1]+25, value[0]-25:value[0]+25]
                 #cv2.imshow('reye',right_eye_img)
                 cv2.rectangle(image,(value[0]-25, value[1]-25), (value[0]+25, value[1]+25), (0, 255, 0), 2)
+                got_right = True
                 break
         
-        if left_eye_img.any() == None or right_eye_img.any() == None:
+        if not( got_left and got_right):
             return None
 
 
 
 
 def main():
-    video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture('C:/Users/User/Documents/GitHub/eye_track_stimulate_process/saved_videos/output_1_pos(x,y)=(-15.19,-26.43).avi')
+    detector = mtcnn.MTCNN()
     while (True):
         _, frame = video_capture.read()
+        if not _ :
+            break
         frame = imutils.resize(frame, width=500)
-        get_data_from_webcam(frame)
+        get_data_from_webcam(frame,detector)
         frame = cv2.resize(frame, (one_size, one_size))
         cv2.imshow("gaze point", frame)
         cv2.waitKey(1)
