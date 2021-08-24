5d4908f143d782b6b77a47e2bdfd575c5492173d
appleRtsan <a29947393@gmail.com>
2021-08-24 11:09:26 +0800

    delete some redundant things

--- openface_mtcnn/testmtcnn.py
+++ openface_mtcnn/testmtcnn.py
@@ -1,24 +1,11 @@
 import os
 import numpy as np
-from PIL import Image
-from imutils import face_utils
 import imutils
 import mtcnn
 import cv2
 
 
 one_size = 500
-
-def get_eye(shape, i, j, image):
-    (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
-    h_buffer = (100 - h)/2  #set image to 104x104
-    w_buffer = (100 - w)/2
-    roi = image[int(y - h_buffer) : int(y + h + h_buffer), int(x - w_buffer) : int(x + w_buffer + w) ]
-    roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
-    # roa = cv2.resize(roi, (224, 224))
-    cv2.rectangle(image, (int( x-w_buffer ), int(y-h_buffer)), (int(x + w_buffer + w), int(y + h + h_buffer)), (0, 255, 0), 2)
-    return roi
-
 
 def get_data_from_webcam(image):
 
@@ -32,10 +19,12 @@ def get_data_from_webcam(image):
     right_eye_img = []
     for face in faces:
         x, y, w, h = face['box']
-        #roi_face = image[y:y + h, x:x + w]
+        if w < 50 or h < 25:
+            continue
+        # roi_face = image[y:y + h, x:x + w]
         # cv2.imshow('face',roi_face)
         # cv2.waitKey(0)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2) #draw face
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2) #draw face
         for key, value in face['keypoints'].items():
             
             if key == 'left_eye':
@@ -48,7 +37,7 @@ def get_data_from_webcam(image):
                 cv2.rectangle(image,(value[0]-25, value[1]-25), (value[0]+25, value[1]+25), (0, 255, 0), 2)
                 break
         
-        if left_eye_img == None or right_eye_img == None:
+        if left_eye_img.any() == None or right_eye_img.any() == None:
             return None
 
 
@@ -56,7 +45,6 @@ def get_data_from_webcam(image):
 
 def main():
     video_capture = cv2.VideoCapture(0)
-    none_counter = 0
     while (True):
         _, frame = video_capture.read()
         frame = imutils.resize(frame, width=500)
